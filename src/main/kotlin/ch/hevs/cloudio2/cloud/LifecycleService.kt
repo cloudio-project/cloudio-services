6a59ae0eed53d555d582bc52ef35fd2d65a45088
lucblender <lucasbonvin@hotmail.com>
2019-05-28 10:37:16 +0200

    Added influx support for lifecycle

--- src/main/kotlin/ch/hevs/cloudio2/cloud/LifecycleService.kt
+++ src/main/kotlin/ch/hevs/cloudio2/cloud/LifecycleService.kt
@@ -5,6 +5,8 @@ import ch.hevs.cloudio2.cloud.model.Node
 import ch.hevs.cloudio2.cloud.repo.EndpointEntity
 import ch.hevs.cloudio2.cloud.repo.EndpointEntityRepository
 import ch.hevs.cloudio2.cloud.serialization.SerializationFormatFactory
+import org.influxdb.InfluxDB
+import org.influxdb.dto.Point
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.core.ExchangeBuilder
 import org.springframework.amqp.core.ExchangeTypes
@@ -13,14 +15,26 @@ import org.springframework.amqp.rabbit.annotation.Exchange
 import org.springframework.amqp.rabbit.annotation.Queue
 import org.springframework.amqp.rabbit.annotation.QueueBinding
 import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.core.env.Environment
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import javax.annotation.PostConstruct
 
 @Service
-class LifecycleService(val endpointEntityRepository: EndpointEntityRepository){
+class LifecycleService(val env: Environment, val influx: InfluxDB, val endpointEntityRepository: EndpointEntityRepository){
 
     companion object {
         private val log = LoggerFactory.getLogger(LifecycleService::class.java)
+    }
+    //get database to write by environment property, has default value
+    val database: String by lazy { env.getProperty("CLOUDIO_INFLUX_DATABASE", "CLOUDIO") }
+
+    @PostConstruct
+    fun initialize()
+    {
+        // for testing only
+        if(!influx.databaseExists(database))
+            influx.createDatabase(database)
     }
 
     @RabbitListener(bindings = [QueueBinding(value= Queue(value = "onlineTopic"),
@@ -37,7 +51,8 @@ class LifecycleService(val endpointEntityRepository: EndpointEntityRepository){
             if (messageFormat != null) {
                 val endpoint = Endpoint()
                 messageFormat.deserializeEndpoint(endpoint, data)
-                endpointIsOnline(endpointId, endpoint)
+                endpointIsOnlineMongo(endpointId, endpoint)
+                endpointIsOnlineInflux(endpointId, endpoint)
             } else {
                 log.error("Unrecognized message format in @online message from $endpointId")
             }
@@ -54,7 +69,8 @@ class LifecycleService(val endpointEntityRepository: EndpointEntityRepository){
         log.info("@offline.*")
 
         try {
-            endpointIsOffline(message.messageProperties.receivedRoutingKey.split(".")[1])
+            endpointIsOfflineMongo(message.messageProperties.receivedRoutingKey.split(".")[1])
+            endpointIsOfflineInflux(message.messageProperties.receivedRoutingKey.split(".")[1])
         } catch (exception: Exception) {
             log.error("Exception during @offline message handling:", exception)
         }
@@ -65,6 +81,7 @@ class LifecycleService(val endpointEntityRepository: EndpointEntityRepository){
             key = ["@nodeAdded.*.nodes.*"])])
     fun handleNodeAddedMessage(message: Message)
     {
+        log.info("@nodeAdded.*.nodes.*")
         try {
             val endpointId = message.messageProperties.receivedRoutingKey.split(".")[1]
             val nodeName = message.messageProperties.receivedRoutingKey.split(".")[3]
@@ -73,9 +90,10 @@ class LifecycleService(val endpointEntityRepository: EndpointEntityRepository){
             if (messageFormat != null) {
                 val node = Node()
                 messageFormat.deserializeNode(node, data)
-                nodeAdded(endpointId, nodeName, node)
+                nodeAddedMongo(endpointId, nodeName, node)
+                nodeAddedInflux(endpointId, nodeName, node)
             } else {
-                log.error("Unrecognized message format in @online message from $endpointId")
+                log.error("Unrecognized message format in @nodeAdded message from $endpointId")
             }
         } catch (exception: Exception) {
             log.error("Exception during @nodeAdded message handling:", exception)
@@ -92,20 +110,21 @@ class LifecycleService(val endpointEntityRepository: EndpointEntityRepository){
         try {
             val endpointId = message.messageProperties.receivedRoutingKey.split(".")[1]
             val nodeName = message.messageProperties.receivedRoutingKey.split(".")[3]
-            nodeRemoved(endpointId, nodeName)
+            nodeRemovedMongo(endpointId, nodeName)
+            nodeRemovedInflux(endpointId, nodeName)
         } catch (exception: Exception) {
             log.error("Exception during @nodeAdded message handling:", exception)
         }
     }
 
-    fun endpointIsOnline(endpointId: String, endpoint: Endpoint) {
+    fun endpointIsOnlineMongo(endpointId: String, endpoint: Endpoint) {
         val endpointEntity = endpointEntityRepository.findByIdOrNull(endpointId) ?: EndpointEntity(endpointId)
         endpointEntity.online = true
         endpointEntity.endpoint = endpoint
         endpointEntityRepository.save(endpointEntity)
     }
 
-    fun endpointIsOffline(endpointId: String) {
+    fun endpointIsOfflineMongo(endpointId: String) {
         val endpointEntity = endpointEntityRepository.findByIdOrNull(endpointId)
         if (endpointEntity != null) {
             endpointEntity.online = false
@@ -113,7 +132,7 @@ class LifecycleService(val endpointEntityRepository: EndpointEntityRepository){
         }
     }
 
-    fun nodeAdded(endpointId: String, nodeName: String, node: Node) {
+    fun nodeAddedMongo(endpointId: String, nodeName: String, node: Node) {
         val endpointEntity = endpointEntityRepository.findByIdOrNull(endpointId)
         if (endpointEntity != null) {
             endpointEntity.endpoint.nodes[nodeName] = node
@@ -121,11 +140,41 @@ class LifecycleService(val endpointEntityRepository: EndpointEntityRepository){
         }
     }
 
-    fun nodeRemoved(endpointId: String, nodeName: String) {
+    fun nodeRemovedMongo(endpointId: String, nodeName: String) {
         val endpointEntity = endpointEntityRepository.findByIdOrNull(endpointId)
         if (endpointEntity != null) {
             endpointEntity.endpoint.nodes.remove(nodeName)
             endpointEntityRepository.save(endpointEntity)
         }
+    }
+
+    fun endpointIsOfflineInflux(endpointId: String) {
+        influx.write(database, "autogen", Point
+                .measurement(endpointId)
+                .addField("event", "offline")
+                .build())
+    }
+
+    fun endpointIsOnlineInflux(endpointId: String, endpoint: Endpoint) {
+        influx.write(database, "autogen", Point
+                .measurement(endpointId)
+                .addField("event", "online")
+                .build())
+    }
+
+    fun nodeAddedInflux(endpointId: String, nodeName: String, node: Node) {
+        influx.write(database, "autogen", Point
+                .measurement(endpointId)
+                .tag("node", nodeName)
+                .addField("event", "added")
+                .build())
+    }
+
+    fun nodeRemovedInflux(endpointId: String, nodeName: String) {
+        influx.write(database, "autogen", Point
+                .measurement(endpointId)
+                .tag("node", nodeName)
+                .addField("event", "removed")
+                .build())
     }
 }
