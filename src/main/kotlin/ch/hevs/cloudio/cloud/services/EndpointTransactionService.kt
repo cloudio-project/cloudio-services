514f0c31c1a1b82584ead0162f4cd5cc920611f6
lucas <lucasbonvin@hotmail.com>
2019-11-22 16:40:42 +0100

    Transactions implementation

---
+++ src/main/kotlin/ch/hevs/cloudio/cloud/services/EndpointTransactionService.kt
@@ -0,0 +1,53 @@
+package ch.hevs.cloudio.cloud.services
+
+import ch.hevs.cloudio.cloud.model.Transaction
+import ch.hevs.cloudio.cloud.serialization.JsonSerializationFormat
+import org.apache.commons.logging.LogFactory
+import org.springframework.amqp.core.ExchangeTypes
+import org.springframework.amqp.core.Message
+import org.springframework.amqp.rabbit.annotation.Exchange
+import org.springframework.amqp.rabbit.annotation.Queue
+import org.springframework.amqp.rabbit.annotation.QueueBinding
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
+
+@Service
+@Profile("transaction", "default")
+class EndpointTransactionService{
+
+    companion object {
+        private val log = LogFactory.getLog(EndpointTransactionService::class.java)
+    }
+
+    @Autowired
+    val rabbitTemplate = RabbitTemplate()
+
+    @RabbitListener(bindings = [QueueBinding(value= Queue(),
+            exchange = Exchange(value = "amq.topic", type = ExchangeTypes.TOPIC, ignoreDeclarationExceptions = "true"),
+            key = ["@transaction.*"])])
+    fun handleTransactionMessage(message: Message)
+    {
+        try {
+            val endpointId = message.messageProperties.receivedRoutingKey.split(".")[1]
+            val data = message.body
+            val messageFormat = JsonSerializationFormat.detect(data)
+            if (messageFormat) {
+                val transaction = Transaction()
+                JsonSerializationFormat.deserializeTransaction(transaction, data)
+
+                transaction.attributes.forEach{ (topic, attribute) ->
+                    rabbitTemplate.convertAndSend("amq.topic",
+                            "@update." + topic.replace("/","."),  JsonSerializationFormat.serializeAttribute(attribute))
+                }
+            }
+            else {
+                log.error("Unrecognized message format in @transaction message from $endpointId")
+            }
+        } catch (exception: Exception) {
+            log.error("Exception during @transaction message handling:", exception)
+        }
+    }
+}
