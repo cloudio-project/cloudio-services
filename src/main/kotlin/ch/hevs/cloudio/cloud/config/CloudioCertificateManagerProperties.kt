e17510e08bab76a1b056ad47be82eaab00d39edd
Michael Clausen <michael.clausen@hevs.ch>
2020-02-11 17:13:44 +0100

    Certificate manager config is only needed for its profile

--- src/main/kotlin/ch/hevs/cloudio/cloud/config/CloudioCertificateManagerProperties.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/config/CloudioCertificateManagerProperties.kt
@@ -2,9 +2,11 @@ package ch.hevs.cloudio.cloud.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.context.annotation.Profile
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "cloudio.cert-manager")
+@Profile("certificate-manager", "default")
 class CloudioCertificateManagerProperties(
         val caCertificate: String,
         val caPrivateKey: String,
