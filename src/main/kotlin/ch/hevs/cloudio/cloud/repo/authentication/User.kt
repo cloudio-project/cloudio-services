9394919209932453dfde2991d5fe4aa206d80521
Michael Clausen <michael.clausen@hevs.ch>
2020-02-15 10:43:01 +0100

    Giving user access to broker and http api per default

--- src/main/kotlin/ch/hevs/cloudio/cloud/repo/authentication/User.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/repo/authentication/User.kt
@@ -12,6 +12,6 @@ data class User(
         var passwordHash: String = "",
         var permissions: Map<String, PrioritizedPermission> = emptyMap(),
         var userGroups: Set<String> = emptySet(),
-        var authorities: Set<Authority> = emptySet(),
+        var authorities: Set<Authority> = setOf(Authority.BROKER_ACCESS, Authority.HTTP_ACCESS),
         var banned: Boolean = false
 )
