b86ce68618fdd66796bb5f1b4c8c465caf7691f9
lucas <lucasbonvin@hotmail.com>
2020-04-09 11:41:29 +0200

    plural for supportedFormats

--- src/main/kotlin/ch/hevs/cloudio/cloud/model/Endpoint.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/model/Endpoint.kt
@@ -2,6 +2,6 @@ package ch.hevs.cloudio.cloud.model
 
 data class Endpoint(
         val version: String = "",
-        val supportedFormat: Set<String> = mutableSetOf(),
+        val supportedFormats: Set<String> = mutableSetOf(),
         val nodes: MutableMap<String, Node> = mutableMapOf()
 )
