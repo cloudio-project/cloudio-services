778c29b31d0997551d90a8320f3b2dbbb46e60a8
Michael Clausen <michael.clausen@hevs.ch>
2020-04-21 10:53:03 +0200

    Fixed CBOR detection

--- src/main/kotlin/ch/hevs/cloudio/cloud/serialization/CBORSerializationFormat.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/serialization/CBORSerializationFormat.kt
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component
 
 @Component
 class CBORSerializationFormat: AbstractJacksonSerializationFormat(Jackson2ObjectMapperBuilder.cbor().build<ObjectMapper>()) {
-    override fun detect(data: ByteArray) = data.isNotEmpty() && data[0].toInt() and 0x10100000 == 0x10100000
+    override fun detect(data: ByteArray) = data.isNotEmpty() && data[0].toInt() and 0x11100000 == 0x10100000
     override fun identifier() = "CBOR"
 }
