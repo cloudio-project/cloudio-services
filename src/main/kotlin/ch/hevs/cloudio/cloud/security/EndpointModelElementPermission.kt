bf3152a04f43d7f335fb8bf67e4f80a8ce0b6dd4
Michael Clausen <michael.clausen@hevs.ch>
2020-05-26 18:51:21 +0200

    Added VIEW permission for elements

--- src/main/kotlin/ch/hevs/cloudio/cloud/security/EndpointModelElementPermission.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/security/EndpointModelElementPermission.kt
@@ -11,15 +11,21 @@ enum class EndpointModelElementPermission(private val value: Int) {
     DENY(0),
 
     /**
+     * The user or group can see the model element and all it's children if not another permission is attributed to one of the children. Note that this does
+     * not include to read the actual value.
+     */
+    VIEW(1),
+
+    /**
      * The user or group can read the model element and all it's children if not another permission is attributed to one of the children.
      */
-    READ(1),
+    READ(2),
 
     /**
      * The user or group can read and write the model element and all it's children if not another permission is attributed to one of the children that
      * overrides this one.
      */
-    WRITE(2);
+    WRITE(3);
 
     /**
      * Returns true if the permission fulfills the passed requirement.
