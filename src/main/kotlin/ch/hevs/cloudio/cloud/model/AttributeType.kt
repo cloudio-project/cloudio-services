1f14d2237dfb83b2a3ce8f5bdf837dd30b6f4c24
Michael Clausen <michael.clausen@hevs.ch>
2020-08-27 09:51:27 +0200

    Added method to check type conformance to AttributeType

--- src/main/kotlin/ch/hevs/cloudio/cloud/model/AttributeType.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/model/AttributeType.kt
@@ -28,5 +28,19 @@ enum class AttributeType {
     /**
      * The attribute is of type string.
      */
-    String
+    String;
+
+    /**
+     * Checks if the passed value object conforms to the type.
+     *
+     * @param value Value to check for type compatibility.
+     * @return      True if the type matches, false otherwise.
+     */
+    fun checkType(value: Any?) = when {
+        this == Boolean && value is kotlin.Boolean -> true
+        this == Integer && (value is Int || value is Long) -> true
+        this == Number && (value is Float || value  is Double) -> true
+        this == String && (value is kotlin.String) -> true
+        else -> false
+    }
 }
