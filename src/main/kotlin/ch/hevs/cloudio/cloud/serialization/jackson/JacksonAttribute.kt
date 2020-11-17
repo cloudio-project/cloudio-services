0ea5955a1d1bb2c926bf427608feede4603406b6
Michael Clausen <michael.clausen@hevs.ch>
2020-11-17 22:39:21 +0100

    Fixed static attribute bug

--- src/main/kotlin/ch/hevs/cloudio/cloud/serialization/jackson/JacksonAttribute.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/serialization/jackson/JacksonAttribute.kt
@@ -15,7 +15,7 @@ data class JacksonAttribute(
     fun toAttribute() = Attribute(
             constraint = constraint.get().also { if (it == AttributeConstraint.Invalid) throw SerializationException() },
             type = type.get().also { if (it == AttributeType.Invalid || !it.checkType(value.orElse(null))) throw SerializationException() },
-            timestamp = timestamp.get().also { if (it < 0) throw SerializationException() },
+            timestamp = if (constraint.get() == AttributeConstraint.Static) null else timestamp.get().also { if (it < 0) throw SerializationException() },
             value = value.get()
     )
 }
