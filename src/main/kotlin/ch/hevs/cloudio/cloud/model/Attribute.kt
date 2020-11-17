2573d200111a0ec6f3b0b463e3dd89bc0100790f
Michael Clausen <michael.clausen@hevs.ch>
2020-11-17 22:38:38 +0100

    Hide missing attribute values and timestamps

--- src/main/kotlin/ch/hevs/cloudio/cloud/model/Attribute.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/model/Attribute.kt
@@ -1,5 +1,7 @@
 package ch.hevs.cloudio.cloud.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
 /**
  * Represents an attribute (data model leaf) of a cloud.iO endpoint's data model. Send by an endpoint on an attribute update (@update) and as port of a @online or @nodeAdded message.
  * @see ActionIdentifier.ATTRIBUTE_UPDATE
@@ -21,11 +23,13 @@ data class Attribute(
          * Timestamp of the last value change (reported to the cloud) of the attribute or timestamp of the set request from the the cloud to the endpoint.
          * UNIX Epoch time in seconds, floating point type allows higher resolution than seconds.
          */
-        var timestamp: Double = -1.0,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        var timestamp: Double? = null,
 
         /**
          * Attribute's value.
          * The type has to match [Attribute.type]
          */
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         var value: Any? = null
 )
