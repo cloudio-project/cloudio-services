31b12a5a8762f97be0635ee3263b761f4ea2407b
Michael Clausen <michael.clausen@hevs.ch>
2021-06-30 09:56:07 +0200

    Removed classes not used anymore

--- src/test/kotlin/ch/hevs/cloudio/cloud/TestUtil.kt
+++ src/test/kotlin/ch/hevs/cloudio/cloud/TestUtil.kt
@@ -1,10 +1,5 @@
 package ch.hevs.cloudio.cloud
 
-import ch.hevs.cloudio.cloud.restapi.admin.usergroup.UserGroupEntity
-import ch.hevs.cloudio.cloud.security.BrokerPermission
-import ch.hevs.cloudio.cloud.security.PermissionPriority
-import ch.hevs.cloudio.cloud.security.PrioritizedPermission
-
 object TestUtil {
 
     private val alphabet: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
