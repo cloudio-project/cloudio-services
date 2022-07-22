d1a804c170b70158c250e220454dd36f506dec25
MartinMeyer1 <meyer.mart@outlook.com>
2022-07-22 14:42:28 +0200

    Add interceptor

--- src/main/kotlin/ch/hevs/cloudio/cloud/services/WebSocketAttributeService.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/services/WebSocketAttributeService.kt
@@ -2,14 +2,23 @@ package ch.hevs.cloudio.cloud.services
 
 import ch.hevs.cloudio.cloud.abstractservices.AbstractAttributeService
 import ch.hevs.cloudio.cloud.model.Attribute
-import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.context.ApplicationListener
+import org.springframework.messaging.Message
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.messaging.simp.annotation.SubscribeMapping
+import org.springframework.messaging.support.ChannelInterceptor
 import org.springframework.stereotype.Controller
+import org.springframework.messaging.simp.stomp.StompCommand
+
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+
+import org.springframework.messaging.MessageChannel
+
+
+
 
 @Controller
-class WebSocketAttributeService(private val template: SimpMessagingTemplate) : AbstractAttributeService()  {
-
+class WebSocketAttributeService(private val template: SimpMessagingTemplate) : AbstractAttributeService(), ChannelInterceptor {
 
     fun sendToClients(attributeId: String, attribute: Attribute) {
         val topic = attributeId.replace(".", "/")
@@ -22,5 +31,20 @@ class WebSocketAttributeService(private val template: SimpMessagingTemplate) : A
 
     override fun attributeSet(attributeId: String, attribute: Attribute) {
         sendToClients(attributeId, attribute)
+    }
+
+    @SubscribeMapping("/**")
+    fun subscribe(topic: String){
+        print(topic)
+    }
+
+
+    override fun preSend(message: Message<*>, channel: MessageChannel): Message<*>? {
+        val accessor = StompHeaderAccessor.wrap(message)
+        val command: StompCommand? = accessor.command
+        if (StompCommand.SUBSCRIBE == command){
+            print("alo")
+        }
+        return message
     }
 }
