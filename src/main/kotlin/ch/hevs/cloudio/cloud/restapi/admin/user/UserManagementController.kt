b1b586dc6bf9e3111bf16b77e829b9b3113dc191
Michael Clausen <michael.clausen@hevs.ch>
2022-09-03 15:45:41 +0200

    Completed documentation for user management controller

--- src/main/kotlin/ch/hevs/cloudio/cloud/restapi/admin/user/UserManagementController.kt
+++ src/main/kotlin/ch/hevs/cloudio/cloud/restapi/admin/user/UserManagementController.kt
@@ -7,13 +7,16 @@ import ch.hevs.cloudio.cloud.dao.UserRepository
 import ch.hevs.cloudio.cloud.restapi.CloudioHttpExceptions
 import ch.hevs.cloudio.cloud.security.Authority
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
@@ -28,26 +31,27 @@ class UserManagementController(
         private var groupRepository: UserGroupRepository,
         private var passwordEncoder: PasswordEncoder
 ) {
-    @GetMapping("/users")
+    @GetMapping("/users", produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "List all users.")
-    @ApiResponse(description = "Success", responseCode = "200", content = [Content(mediaType = "application/json", array = ArraySchema(schema = Schema(implementation = ListUserEntity::class)))])
-    fun getAllUsers() = userRepository.findAll().map {
-        ListUserEntity(
-                name = it.userName,
-                email = it.emailAddress.toString(),
-                authorities = it.authorities,
-                banned = it.banned
-        )
-    }
+    @ApiResponses(value = [
+        ApiResponse(description = "List of all users.", responseCode = "200", content = [Content(mediaType = "application/json", array = ArraySchema(schema = Schema(implementation = ListUserEntity::class)))]),
+        ApiResponse(description = "Forbidden.", responseCode = "403", content = [Content()])
+    ])
+    fun getAllUsers() = userRepository.findAll().map { ListUserEntity(it) }
 
-    @PostMapping("/users")
+    @PostMapping("/users",  consumes = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
     @Operation(summary = "Create a new user.")
-    @ApiResponse(description = "User was created", responseCode = "204")
-    @ApiResponse(description = "User with same username exists", responseCode = "409")
-    fun createUser(@RequestBody body: PostUserEntity) {
+    @ApiResponses(value = [
+        ApiResponse(description = "User was created", responseCode = "204"),
+        ApiResponse(description = "User with same username exists", responseCode = "409"),
+        ApiResponse(description = "Forbidden.", responseCode = "403", content = [Content()])
+    ])
+    fun postUser(
+        @RequestBody body: PostUserEntity
+    ) {
         if (userRepository.existsByUserName(body.name)) {
             throw CloudioHttpExceptions.Conflict("User '${body.name}' exists.")
         }
@@ -59,42 +63,46 @@ class UserManagementController(
                     }
                 },
                 password = passwordEncoder.encode(body.password),
-                authorities = body.authorities.toMutableSet(),
-                banned = body.banned,
-                groupMemberships = body.groupMemberships.map {
+                authorities = (body.authorities ?: Authority.DEFAULT_AUTHORITIES).toMutableSet(),
+                banned = body.banned ?: false,
+                groupMemberships = (body.groupMemberships ?: emptySet()).map {
                     groupRepository.findByGroupName(it).orElseThrow {
                         CloudioHttpExceptions.NotFound("Group '$it' does not exist.")
                     }
                 }.toMutableSet(),
-                metaData = body.metaData.toMutableMap()
+                metaData = (body.metaData ?: emptyMap()).toMutableMap()
         ))
     }
 
-    @Operation(summary = "Get user information.")
-    @GetMapping("/users/{userName}")
+    @GetMapping("/users/{userName}", produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseStatus(HttpStatus.OK)
     @Transactional(readOnly = true)
-    fun getUserByUserName(@PathVariable userName: String) = userRepository.findByUserName(userName).orElseThrow {
+    @Operation(summary = "Get user information.")
+    @ApiResponses(value = [
+        ApiResponse(description = "User details.", responseCode = "200", content = [Content(mediaType = "application/json", schema = Schema(implementation = UserEntity::class))]),
+        ApiResponse(description = "User not found.", responseCode = "404", content = [Content()]),
+        ApiResponse(description = "Forbidden.", responseCode = "403", content = [Content()])
+    ])
+    fun getUserByUserName(
+        @PathVariable @Parameter(description = "Username") userName: String
+    ) = userRepository.findByUserName(userName).orElseThrow {
         CloudioHttpExceptions.NotFound("User '$userName' not found.")
-    }.run {
-        UserEntity(
-                name = userName,
-                email = emailAddress.toString(),
-                authorities = authorities,
-                banned = banned,
-                groupMemberships = groupMemberships.map { it.groupName }.toSet(),
-                metadata = metaData
-        )
-    }
+    }.let { UserEntity(it) }
 
-    @Operation(summary = "Modify user information.")
-    @PutMapping("/users/{userName}")
+    @PutMapping("/users/{userName}", consumes = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
-    fun updateUserByUserName(@PathVariable userName: String, @RequestBody body: UserEntity) {
-        if (userName != body.name) {
-            throw CloudioHttpExceptions.BadRequest("User name in URL and body do not match.")
-        }
+    @Operation(summary = "Modify user information.")
+    @ApiResponses(value = [
+        ApiResponse(description = "User information modified.", responseCode = "204", content = [Content()]),
+        ApiResponse(description = "User not found.", responseCode = "404", content = [Content()]),
+        ApiResponse(description = "Forbidden.", responseCode = "403", content = [Content()])
+    ])
+    fun updateUserByUserName(
+        @PathVariable @Parameter(description = "Username") userName: String,
+        @RequestBody body: UserEntity
+    ) {
+        body.name = userName
         userRepository.findByUserName(userName).orElseThrow {
             CloudioHttpExceptions.NotFound("User '$userName' not found.")
         }.run {
@@ -108,36 +116,51 @@ class UserManagementController(
                 addAll(body.authorities)
             }
             banned = body.banned
-            groupMemberships.clear()
-            groupMemberships.addAll(body.groupMemberships.map {
-                groupRepository.findByGroupName(it).orElseThrow {
-                    CloudioHttpExceptions.NotFound("Group '$it' does not exist.")
-                }
-            })
+            groupMemberships.apply {
+                clear()
+                addAll(body.groupMemberships.map {
+                    groupRepository.findByGroupName(it).orElseThrow {
+                        CloudioHttpExceptions.NotFound("Group '$it' does not exist.")
+                    }
+                })
+            }
             metaData = body.metadata.toMutableMap()
             userRepository.save(this)
         }
     }
 
-    @Operation(summary = "Change user's password.")
     @PutMapping("/users/{userName}/password")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun changeUserPassword(@PathVariable userName: String, @RequestParam password: String) {
+    @Operation(summary = "Change user's password.")
+    @ApiResponses(value = [
+        ApiResponse(description = "User password changed.", responseCode = "204", content = [Content()]),
+        ApiResponse(description = "User not found.", responseCode = "404", content = [Content()]),
+        ApiResponse(description = "Forbidden.", responseCode = "403", content = [Content()])
+    ])
+    fun changeUserPassword(
+        @PathVariable userName: String,
+        @RequestParam password: String
+    ) {
         userRepository.findByUserName(userName).orElseThrow {
-            CloudioHttpExceptions.NotFound("Can not change user's password - User '$userName' not found.")
+            CloudioHttpExceptions.NotFound("User '$userName' not found.")
         }.let {
             it.password = passwordEncoder.encode(password)
             userRepository.save(it)
         }
     }
 
-    @Operation(summary = "Delete user.")
     @DeleteMapping("/users/{userName}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
+    @Operation(summary = "Delete user.")
+    @ApiResponses(value = [
+        ApiResponse(description = "User deleted.", responseCode = "204", content = [Content()]),
+        ApiResponse(description = "User not found.", responseCode = "404", content = [Content()]),
+        ApiResponse(description = "Forbidden.", responseCode = "403", content = [Content()])
+    ])
     fun deleteUserByUserName(@PathVariable userName: String) {
         if (!userRepository.existsByUserName(userName)) {
-            throw CloudioHttpExceptions.NotFound("Can not delete user - User '$userName' not found.")
+            throw CloudioHttpExceptions.NotFound("User '$userName' not found.")
         }
         userRepository.deleteByUserName(userName)
     }
