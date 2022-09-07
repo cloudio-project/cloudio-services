b05e8e122c49c11e98c72ef4e776971e66dba984
MartinMeyer1 <meyer.mart@outlook.com>
2022-09-07 08:52:17 +0200

    Forgot to add endpointGroupListEntity

---
+++ src/main/kotlin/ch/hevs/cloudio/cloud/restapi/endpoint/group/EndpointGroupListEntity.kt
@@ -0,0 +1,14 @@
+package ch.hevs.cloudio.cloud.restapi.endpoint.group
+
+import ch.hevs.cloudio.cloud.security.EndpointPermission
+import io.swagger.v3.oas.annotations.media.Schema
+import java.util.*
+
+@Schema(name = "EndpointGroupListEntry", description = "Endpoint group overview used in endpoint group listing.")
+data class EndpointGroupListEntity(
+        @Schema(description = "The endpoint group name", readOnly = true)
+        val name: String,
+
+        @Schema(description = "The permission the currently authenticated user has for this endpoint group.", readOnly = true, example = "OWN")
+        val permission: EndpointPermission
+)
